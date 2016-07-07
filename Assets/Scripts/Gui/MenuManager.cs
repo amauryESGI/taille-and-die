61c08cba91d6ebec98574dfca5739c11ed56f124
amauryESGI <amaury.colmant@wanadoo.fr>
2016-07-07 12:13:33 +0200

    Add Lobby scene & default mecanique UI

---
+++ Assets/Scripts/Gui/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GUI
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public Menu CurrentMenu;
+
+        public void Start()
+        {
+            ShowMenu(CurrentMenu);
+        }
+        public void ShowMenu(Menu menu)
+        {
+            HideCurrentMenu();
+
+            CurrentMenu = menu;
+            CurrentMenu.IsOpen = true;
+        }
+        public void HideCurrentMenu()
+        {
+            if ( CurrentMenu != null )
+                CurrentMenu.IsOpen = false;
+            CurrentMenu = null;
+        }
+        public void DebugMenu(Menu menu) {
+            Debug.Log("Ask to display menu : \"" + menu.name + "\".");
+        }
+    }
+}
