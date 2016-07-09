8d3e7ea90c6e0d2cbf2f75121e38c70c9d9b00b1
amauryESGI <amaury.colmant@wanadoo.fr>
2016-07-09 16:33:47 +0200

    Add first menu editor

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI.Misc {
+    public class Game : MonoBehaviour {
+        [SerializeField]
+        private List<Button> _buttonsExitGame;
+
+        void Awake() {
+            if (Application.isWebPlayer || Application.isEditor)
+                _buttonsExitGame.ForEach(buttonExitGame => buttonExitGame.interactable = false);
+        }
+
+        public void OnJoinGame(string loadLevel) {
+            Application.LoadLevel(loadLevel);
+        }
+
+        public void OnExitGame() {
+            Application.Quit();
+        }
+    }
+}
