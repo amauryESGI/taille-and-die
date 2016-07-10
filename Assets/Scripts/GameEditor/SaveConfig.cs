07104e2beeb56a75e15998e6600a6e7de1f533c4
amauryESGI <amaury.colmant@wanadoo.fr>
2016-07-10 14:31:52 +0200

    Add save of level edit

---
+++ Assets/Scripts/GameEditor/SaveConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class SaveConfig {
+    public static string dataPath;
+
+    static SaveConfig() {
+        dataPath = Application.dataPath;
+        dataPath = dataPath.Replace("Assets", "");
+    }
+}
