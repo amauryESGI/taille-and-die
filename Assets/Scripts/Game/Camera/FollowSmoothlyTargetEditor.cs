902f2897e682b21349ab74139b2dbb05191babbf
Aurelien GILLE <aurelien.gille@gmail.com>
2016-07-12 12:19:54 +0200

    Edit test IA SCENES

--- Assets/Scripts/Game/Camera/FollowSmoothlyTargetEditor.cs
+++ Assets/Scripts/Game/Camera/FollowSmoothlyTargetEditor.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
-using UnityEditor;
+//using UnityEditor;
 
-[CustomEditor(typeof(FollowSmoothlyTarget))]
-public class FollowSmoothlyTargetEditor : Editor {
-    public override void OnInspectorGUI() {
-        DrawDefaultInspector();
+//[CustomEditor(typeof(FollowSmoothlyTarget))]
+//public class FollowSmoothlyTargetEditor : Editor {
+//    public override void OnInspectorGUI() {
+//        DrawDefaultInspector();
 
-        FollowSmoothlyTarget fst = (FollowSmoothlyTarget)target;
+//        FollowSmoothlyTarget fst = (FollowSmoothlyTarget)target;
 
-        if (fst.IsBoundable()) {
-            if (GUILayout.Button("Set Bound Min"))
-                fst.SetMinPos();
-            if (GUILayout.Button("Set Bound Max"))
-                fst.SetMaxPos();
-        }
-    }
-}
+//        if (fst.IsBoundable()) {
+//            if (GUILayout.Button("Set Bound Min"))
+//                fst.SetMinPos();
+//            if (GUILayout.Button("Set Bound Max"))
+//                fst.SetMaxPos();
+//        }
+//    }
+//}
