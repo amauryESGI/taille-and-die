ad1ec1c3b7ba7ead419cd9af1114d307f955b8c8
amauryESGI <amaury.colmant@wanadoo.fr>
2016-10-06 15:54:16 +0200

    Fix end in editor

--- Assets/Scripts/Game/GameArrive.cs
+++ Assets/Scripts/Game/GameArrive.cs
@@ -5,8 +5,10 @@ public class GameArrive : MonoBehaviour {
     [SerializeField] private string _sceneWin;
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("WIN !");
-        GameStat.Win();
-        SceneManager.LoadScene(_sceneWin);
+        if (Application.loadedLevelName != "EditorMap") {
+            Debug.Log("WIN !");
+            GameStat.Win();
+            SceneManager.LoadScene(_sceneWin);
+        }
     }
 }
